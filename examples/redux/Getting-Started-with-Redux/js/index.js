7a13a2ed1af90030827ff98c114e04578a691c5a
zsimo <zsimo30@gmail.com>
2016-12-19 23:26:18 +0100

    added redux

--- examples/redux/Getting-Started-with-Redux/js/index.js
+++ examples/redux/Getting-Started-with-Redux/js/index.js
@@ -2,19 +2,54 @@
  * Created by simonesacchi on 19/12/2016.
  */
 
+"use strict";
 
 // reduces
-function counter (state, action) {
-    if (action.type === "INCREMENT") {
+function counter(state, action) {
+
+    // if the state is undefined, returns the initial state of the application
+    if (typeof state === 'undefined') {
+        return 0;
+    }
+    if (action.type === 'INCREMENT') {
         return state + 1;
-    } else if (action.type === "DECREMENT") {
+    } else if (action.type === 'DECREMENT') {
         return state - 1;
+    } else {
+        return state;
     }
 }
 
-expect(counter(0, {type : "INCREMENT"})).toEqual(1);
-expect(counter(1, {type : "INCREMENT"})).toEqual(2);
-expect(counter(2, {type : "DECREMENT"})).toEqual(1);
-expect(counter(1, {type : "DECREMENT"})).toEqual(0);
 
-console.log("test passed!");
+function render () {
+    // update UI
+    console.log(store.getState());
+}
+
+
+// pass to the store the reducer
+var store = Redux.createStore(counter);
+// .getState()
+// .dispatch()
+// .subscribe()
+
+
+document.getElementById("button").addEventListener("click", function () {
+    store.dispatch({type : "INCREMENT"});
+});
+store.subscribe(render);
+// render initial state once
+render();
+
+expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
+
+// custom action returns the same state
+expect(counter(1, { type: 'CUSTOM' })).toEqual(1);
+
+// custom action returns the same state
+expect(counter(undefined, {})).toEqual(0);
+
+console.log('test passed!');
