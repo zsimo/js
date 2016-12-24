52d9f5e9c26919250ebb071d0cf28dd3f7235d5d
Simone Sacchi <simone.sacchi@nubilaria.com>
2016-12-24 10:41:07 +0100

    added babel on rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,16 @@
  * Created by Simone.Sacchi on 23/12/2016.
  */
 
+import babel from "rollup-plugin-babel";
 
 export default {
     entry : "examples/rollup/src/js/index.js",
     dest : "examples/rollup/public/js/index.js",
-    format : "iife"
-}
+    format : "iife",
+    sourceMap : "inline",
+    plugins : [
+        babel ({
+            exclude : "node_modules/**"
+        })
+    ]
+};
