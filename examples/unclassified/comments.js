299fa76de2e1ce4dfab2f999c5871a0b0cd4cca4
zsimo <zsimo30@gmail.com>
2017-01-06 09:24:17 +0100

    added comment

---
+++ examples/unclassified/comments.js
@@ -0,0 +1,16 @@
+/**
+ * Created by simonesacchi on 06/01/2017.
+ */
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Initialization
+ |--------------------------------------------------------------------------
+ |
+ | As our first step, we'll require the project's Laravel Mix file
+ | and record the user's requested compilation and build steps.
+ | Once those steps have been recorded, we may get to work.
+ | https://github.com/JeffreyWay/laravel-mix/blob/master/setup/webpack.config.js
+ |
+ */
