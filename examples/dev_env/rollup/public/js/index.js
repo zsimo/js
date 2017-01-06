b79b4d4a7853b7c19e49a9af0087d0f753362497
Simone Sacchi <simone.sacchi@nubilaria.com>
2017-01-06 15:12:22 +0100

    first webpack_babel_liveserver implementation

---
+++ examples/dev_env/rollup/public/js/index.js
@@ -0,0 +1,11 @@
+(function () {
+'use strict';
+
+/**
+ * Created by Simone.Sacchi on 24/12/2016.
+ */
+
+console.log("ok");
+
+}());
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjpudWxsLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9qcy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcbi8qKlxuICogQ3JlYXRlZCBieSBTaW1vbmUuU2FjY2hpIG9uIDI0LzEyLzIwMTYuXG4gKi9cblxuXG5jb25zb2xlLmxvZyhcIm9rXCIpO1xuXG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7O0FBQ0E7Ozs7QUFLQUEsUUFBUUMsR0FBUixDQUFZLElBQVo7OyJ9
