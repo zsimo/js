ad7ce9c4698328cd5f6e27d239d9cafbdf8db946
Simone Sacchi <simone.sacchi@nubilaria.com>
2017-02-21 17:44:33 +0100

    add del-cli

--- examples/dev_env/webpack_tips/tips.js
+++ examples/dev_env/webpack_tips/tips.js
@@ -19,3 +19,8 @@ plugins: [
 new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
 }),
+
+
+
+// use  del-cli
+// "prebuild" : "del-cli dist -f"
