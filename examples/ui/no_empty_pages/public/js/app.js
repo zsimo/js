7a91ed132b5949a8e27461ef260d73934636501d
zsimo <zsimo30@gmail.com>
2017-03-11 00:32:58 +0100

    finish uno and due with resize event

---
+++ examples/ui/no_empty_pages/public/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by simonesacchi on 11/03/2017.
+ */
+
+
+"use strict";
+
+
+
+// handle event
+window.addEventListener("optimizedResize", function() {
+    draw();
+});
+
+draw();
---
+++ examples/ui/no_empty_pages/public/js/lib.js
@@ -0,0 +1,23 @@
+/**
+ * Created by simonesacchi on 11/03/2017.
+ */
+
+"use strict";
+// https://developer.mozilla.org/en-US/docs/Web/Events/resize
+(function() {
+    var throttle = function(type, name) {
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(function() {
+                window.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        window.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
