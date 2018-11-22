60b9f0f282a3686524d6ff6570f72b7a3d828de0
Simone <simone.sacchi@nubilaria.com>
2018-11-22 23:37:20 +0100

    add comment

--- examples/programming/Learning JavaScript Design Patterns/patterns/creational/modules/unique_2/index.js
+++ examples/programming/Learning JavaScript Design Patterns/patterns/creational/modules/unique_2/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 
+// Object.create returns always an EMPTY object, with the prototype bounds to the
+// object passed as parameter
 var obj1 = Object.create(require("./obj"));
 var obj2 = Object.create(require("./obj"));
 obj1.increment();
